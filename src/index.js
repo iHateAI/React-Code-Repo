33039511ba345ee34cf0ab5285d58853ea3a9500
HyunSeong <bolton7425@gmail.com>
2023-07-17 19:45:06 +0900

    05. Hooks

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import Library from "./ch03_JSX/Library";
 import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
 import NotificationList from './chapter_06/NotificationList';
+import Accomodate from './chapter_07/Accomodate';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accomodate />
   </React.StrictMode>
 );
 
