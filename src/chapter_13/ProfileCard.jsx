2ec2109e64a439ee7fb97b1f7caab6ad2b2e4333
HyunSeong <bolton7425@gmail.com>
2023-07-18 03:49:15 +0900

    11. Composition

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title={"ABCD"} backgroundColor={"#4ea04e"}>
+      <p>안녕하세요</p>
+      <p>리액트 너무 좋아</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Library from "./ch03_JSX/Library";
 import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
 import NotificationList from "./chapter_06/NotificationList";
@@ -13,12 +12,13 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
