677e106fa6154ceef7f35c28369bbfd7fce0c6f2
HyunSeong <bolton7425@gmail.com>
2023-07-17 20:37:35 +0900

    08. List And Key

---
+++ src/chapter_10/AttendanceBook.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const students = [
+  {
+    id: 1,
+    name: "A",
+  },
+  {
+    
+    id: 2,
+    name: "B",
+  },
+  {
+    id: 3,
+    name: "C",
+  },
+  {
+    id: 4,
+    name: "D",
+  },
+];
+
+function AttendanceBook(props) {
+  return (
+    <ul>
+      {students.map((student) => {
+        return <li key={student.id}>{student.name}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default AttendanceBook;
